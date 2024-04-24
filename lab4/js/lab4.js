7eda7b694f9060d00c7331af263847f1cd99201e
NolanHunt <128653893+NolanHunt@users.noreply.github.com>
2024-04-23 20:48:09 -0700

    lab 4
    pseudo-coding application

---
+++ lab4/js/lab4.js
@@ -0,0 +1,31 @@
+// index.js - Pseudocoding: Tic, Tac Toe
+// Author: Nolan Hunt <noahunt@ucsc.edu>
+// Date: 24 April 2024
+
+// 1. Set up a game board.
+  // Construct a grid 3x3 for board.
+// 2. Target Player for "X" side and Player for "0" side.
+// 3. Show the initial empty board.
+// 4. Player "X" is prompted to make a move.
+// 5. Player "X's" move confirm.
+  // Confirm if the selected square is empty.
+// 6. Board state is updated with Player "X's" confirm move.
+// 7. Winning condition or a draw is checked for.
+  // Check rows, colums and diagonals for three matching symbols
+    // Check if the board is filled without a winner.
+// 8. It becomes Player "X's" turn if a draw and/or no winner is not confirmed (boolean)
+// 9. Player "O"'s move confirm.
+  // Display a message prompting Player "O" to make a move.
+// 10. Player "O's" move confirm.
+  // Check if the selected square is empty.
+// 11. Update the board with Player "O's" move.
+// 12. Check for a winning or a draw condition.
+// 13. Repeat steps 4-12 until there is a winner or draw.
+// 14. Display the winner or declare a draw.
+  // Display a message indicating the winner or declaring a draw.
+// 15. Ask players if they want to play again.
+  // Display a message asking if players want to play again and take into account input/clicks.
+// 16. Reset the game board if players choose to play again.
+  // Clear the board and reset game state.
+// 17. Game is ended if players press "no" to start again.
+  // Create a farewell message and leave the game. 
