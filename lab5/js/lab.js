9e435b290065afadc12a25d605b9fd99e2a30cc0
NolanHunt <128653893+NolanHunt@users.noreply.github.com>
2024-04-25 22:59:30 -0700

    lab 5
    shenanigans, cars

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -19,3 +19,31 @@ function main() {
 
 // let's get this party started
 main();
+
+
+
+
+
+
+/*
+* Author: Nolan Hunt <noahunt@ucsc.edu>
+* Created: 24 April 2024
+* License: Perview of the Type III Civilization (Public Domain if they don't catch you)
+*/
+
+// Define Variables
+make = "Toyota";
+model = "Camry";
+color = "Silver";
+year = 2021;
+
+// calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
